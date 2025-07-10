6da5760097bd5f8d214dd07b6823515e9b4f8cdc
Ashwin Ram Venkataraman <82612992+ashhhwin@users.noreply.github.com>
2025-07-10 18:41:49 -0400

    Update gradio_app_new.py removed debug prints and sleep timer

--- gradio_app_new.py
+++ gradio_app_new.py
@@ -231,10 +231,10 @@ def run_comparison(from_date, to_date, period):
     safe_period = str(period).replace(' ', '').replace('/', '').replace('\\', '').replace(':', '')
     output_file = f"eps_revenue_comparison_{from_date}_to_{to_date}_for_{safe_period}.csv"
     compare_eps_revenue(from_date=from_date, to_date=to_date, quarters=[period] if period else None, output_file=output_file, annual=False)
-    print("I AM HERE")
+    #print("I AM HERE")
     gcs_path = f'market_data/revisions/{output_file}'
-    print(f"Reading from GCS path: {gcs_path}")
-    time.sleep(20)
+    #print(f"Reading from GCS path: {gcs_path}")
+    #time.sleep(20)
     df = read_csv_from_gcs('historical_data_evoke', gcs_path)
     #df = read_csv_from_gcs('historical_data_evoke',f'market_data/revisions/{output_file}')
     if df is None or df.empty:
