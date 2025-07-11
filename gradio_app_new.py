a225a8af053277b6c14fc4acb3f820688f62e2ba
Ashwin Ram Venkataraman <82612992+ashhhwin@users.noreply.github.com>
2025-07-11 09:30:40 -0400

    Update gradio_app_new.py changed gradio port no

--- gradio_app_new.py
+++ gradio_app_new.py
@@ -511,4 +511,4 @@ with gr.Blocks(theme=gr.themes.Soft()) as app:
 
 ##ashwin changes end here
 
-app.launch(server_name="0.0.0.0", server_port=7860)
+app.launch(server_name="0.0.0.0", server_port=7861)
