01772222db2bbd151f07ab6b3f1ff988627b4e5a
Ashwin Ram Venkataraman <82612992+ashhhwin@users.noreply.github.com>
2025-07-11 09:33:41 -0400

    Update gradio_app_new.py debug 2

--- gradio_app_new.py
+++ gradio_app_new.py
@@ -346,7 +346,7 @@ def load_news_from_gcs(date_str, ticker, bucket_name="historical_data_evoke"):
     """
     Load news JSON from GCS for a given date and ticker.
     """
-    blob_path = f"news/{date_str}/{ticker.upper()}.json"
+    blob_path = f"market_data/news/{date_str}/{ticker.upper()}.json"
     print(blob_path)
     try:
         client = storage.Client()
