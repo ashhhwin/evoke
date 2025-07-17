e0c49e2ef119eea58848c14d05472d0fe84debfa
Anubuthi K <79791514+Anubuthi@users.noreply.github.com>
2025-07-17 13:57:33 -0400

    Update test_con.py

--- test_con.py
+++ test_con.py
@@ -6,7 +6,8 @@ try:
         'SERVER=34.58.50.83,1433;'
         'DATABASE=eodhd_data;'
         'UID=sqlserver;PWD=EvokeIntern@2025;',
-        'Encrypt=no',
+        'Encrypt=no;',
+        'TrustServerCertificate=yes;',
         timeout=5
     )
     print("✅ Connected to SQL Server!")
