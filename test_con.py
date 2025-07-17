02b79c2a55fe160b4ae8fd8ebff911a995747b92
Anubuthi K <79791514+Anubuthi@users.noreply.github.com>
2025-07-17 13:55:37 -0400

    Update test_con.py

--- test_con.py
+++ test_con.py
@@ -5,8 +5,8 @@ try:
         'DRIVER={ODBC Driver 17 for SQL Server};'
         'SERVER=34.58.50.83,1433;'
         'DATABASE=eodhd_data;'
-        'UID=sqlserver;PWD=EvokeIntern@2025',
-        'Encrypt=no',
+        'UID=sqlserver;PWD=EvokeIntern@2025;',
+        'Encrypt=no;',
         timeout=5
     )
     print("✅ Connected to SQL Server!")
