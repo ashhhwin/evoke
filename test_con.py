a38e8f5338f7aea523bd330a2261b71dfc376ca8
Anubuthi K <79791514+Anubuthi@users.noreply.github.com>
2025-07-17 13:58:50 -0400

    Update test_con.py

--- test_con.py
+++ test_con.py
@@ -5,8 +5,9 @@ try:
         'DRIVER={ODBC Driver 17 for SQL Server};'
         'SERVER=34.58.50.83,1433;'
         'DATABASE=eodhd_data;'
-        'UID=sqlserver;PWD=EvokeIntern@2025;',
-        'Encrypt=no;',
+        'UID=sqlserver;'
+        'PWD=EvokeIntern@2025;'
+        'Encrypt=no;'
         'TrustServerCertificate=yes;',
         timeout=5
     )
