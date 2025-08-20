cf88b4df9f91b5d46fcd628f33939e4c8a78824c
Anubuthi K <79791514+Anubuthi@users.noreply.github.com>
2025-08-20 09:05:18 -0400

    Update script.py fixed line 417

--- script.py
+++ script.py
@@ -414,7 +414,7 @@ def run_finnhub_data_pipeline(tickers: List[str]):
                  #       gcs_dest = gcs_path(f"{news_dir}/{tk}.json")
                  #       upload_string_to_gcs("historical_data_evoke", gcs_dest, json_str)          
                  #else:
-                if not df.empty:
+                 if not df.empty:
                     df.insert(0, "ticker", tk)
                     df.insert(1, "api_run_date", today_iso)
                     collected[name].append(df)
