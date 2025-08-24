5868446a490e9083f4e8abfa0fd9b6b55642f037
Ashwin Ram Venkataraman <82612992+ashhhwin@users.noreply.github.com>
2025-08-24 16:57:15 -0400

    august 20th date change

--- script.py
+++ script.py
@@ -546,7 +546,7 @@ def run_finnhub_data_pipeline(tickers: List[str]):
 
 def run_daily_bulk_download(tickers: List[str]):
     
-    date_str = "2025-07-22"
+    date_str = "2025-08-20"
     today = datetime.strptime(date_str, "%Y-%m-%d").date()
     #today= date.today()
     #date_str = today.isoformat()
