bbc2ceb9176da41b98d42df0d150a8697d445ecc
Ashwin Ram Venkataraman <82612992+ashhhwin@users.noreply.github.com>
2025-08-25 10:12:08 -0400

    Update gradio_app_new.py
    added debug flag

--- gradio_app_new.py
+++ gradio_app_new.py
@@ -1633,7 +1633,8 @@ with gr.Blocks(theme=gr.themes.Soft()) as app:
             inputs=[from_cal, to_cal, ticker_input],
             outputs=[status_box, calendar_output]
         )
-app.launch(server_name="0.0.0.0", server_port=7886, pwa=True)
+app.launch(server_name="0.0.0.0", server_port=7886, pwa=True, debug=True)
+
 
 
 
