3345ea350b31b7a6c9d1da7ed2b97a4fbe79b53e
Ashwin Ram Venkataraman <82612992+ashhhwin@users.noreply.github.com>
2025-08-26 15:01:39 -0400

    Update gradio_app_new.py

--- gradio_app_new.py
+++ gradio_app_new.py
@@ -1966,15 +1966,6 @@ with gr.Blocks(theme=gr.themes.Soft()) as app:
             outputs=[status_box, calendar_output]
         )
 
-app.launch(server_name="0.0.0.0", server_port=7888, pwa=True, debug=True)
-
-
-
-
-
-
-
-
-
+#app.launch(server_name="0.0.0.0", server_port=7888, pwa=True, debug=True)
 
 
