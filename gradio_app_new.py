7c68e4a985c87ec57d7000331dcb74ac99e2ce01
Ashwin Ram Venkataraman <82612992+ashhhwin@users.noreply.github.com>
2025-08-28 11:10:31 -0400

    Update gradio_app_new.py

--- gradio_app_new.py
+++ gradio_app_new.py
@@ -2094,4 +2094,5 @@ with gr.Blocks(theme=gr.themes.Soft()) as app:
             outputs=[report_display]
         )
 
-#app.launch(server_name="0.0.0.0", server_port=7888, debug=True)
+app.launch(server_name="0.0.0.0", server_port=7888, debug=True)
+
