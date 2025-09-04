b596cb670cda699dce475bc96d15c1fc80b54d36
Ashwin Ram Venkataraman <82612992+ashhhwin@users.noreply.github.com>
2025-09-04 19:12:30 -0400

    Create run_gradio_server.py

---
+++ run_gradio_server.py
@@ -0,0 +1,9 @@
+from gradio_app_new import app as gradio_app
+
+if __name__ == "__main__":
+    gradio_app.launch(
+        server_name="0.0.0.0",
+        server_port=7869,
+        share=False,
+        debug=False
+    )
